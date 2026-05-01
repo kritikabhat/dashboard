464165ec8772b31e5bdb64cadf85c1a9cc6e3045
kritika <34717469+kritikabhat@users.noreply.github.com>
2026-05-01 00:24:43 -0400

    feat: improved card design- left border and box shadow with blur effect

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /**
- * No script to be used, so delete this later.
- * 
- * Import Roboto font later
- * Import and add icons later
  * Add links to previous projects and later update this with more projects
+ * delete unused icons from images.
  * 
- * Change the px gaps to em or rem
- * 
- * make sure to check at 100% zoom.
  * make it adaptive so it can accept more projects in the future
+ * auto-fit feature thing
+ * change buttons to links
+ * 
+ * add focus opacity thing when you focus on each project
+ * 
+ * make the left sidebar collapsable
  * 
  * see what it looks like on mobile
  */
